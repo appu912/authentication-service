b567a9f3e2f783c9a67171a9b9d3507ceacdf2d3
Apoorv Mehra <mehra.appu912@gmail.com>
2026-02-01 15:17:35 +0000

    Updated schema details

--- data/auth/create-auth-credentials-table.sql
+++ data/auth/create-auth-credentials-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE auth.user_credentials (
     user_id uuid PRIMARY KEY,
-    email text NOT NULL UNIQUE,
-    hashed_password text NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
+    hashed_password varchar(255) NOT NULL,
     created_at timestamptz NOT NULL default now(),
     updated_at timestamptz NOT NULL default now()
 );
